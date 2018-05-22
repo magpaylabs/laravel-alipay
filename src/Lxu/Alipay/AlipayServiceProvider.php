06dab71d6b0d2f4193b1250510c25cbc66bd6626
taras <taras@magpaylabs.com>
2018-05-22 18:09:59 +0300

    fix dir

--- src/Lxu/Alipay/AlipayServiceProvider.php
+++ src/Lxu/Alipay/AlipayServiceProvider.php
@@ -37,7 +37,7 @@ class AlipayServiceProvider extends ServiceProvider{
 
             $pcAlipay = new PcSdk();
             $pcAlipay->partner       = config('lxu-alipay.pcconfig.partner');
-            $pcAlipay->cacert        = __DIR__.'\\config\\cacert.pem';
+            $pcAlipay->cacert        = __DIR__.'/config/cacert.pem';
             $pcAlipay->key           = config('lxu-alipay.pcconfig.key');
             $pcAlipay->sign_type     = config('lxu-alipay.pcconfig.sign_type');
             $pcAlipay->input_charset = config('lxu-alipay.pcconfig.input_charset');
@@ -49,12 +49,12 @@ class AlipayServiceProvider extends ServiceProvider{
 
             $mobileAlipay = new MobileSdk();
             $mobileAlipay->partner              = config('lxu-alipay.mobileconfig.partner');
-            $mobileAlipay->cacert               = __DIR__.'\\config\\cacert.pem';
+            $mobileAlipay->cacert               = __DIR__.'/config/cacert.pem';
             $mobileAlipay->key                  = config('lxu-alipay.mobileconfig.key');
             $mobileAlipay->sign_type            = config('lxu-alipay.mobileconfig.sign_type');
             $mobileAlipay->input_charset        = config('lxu-alipay.mobileconfig.input_charset');
-            $mobileAlipay->ali_public_key_path  = __DIR__.'\\config\\key\\rsa_public_key.pem';
-            $mobileAlipay->private_key_path     = __DIR__.'\\config\\key\\rsa_private_key.pem';
+            $mobileAlipay->ali_public_key_path  = __DIR__.'/config/key/rsa_public_key.pem';
+            $mobileAlipay->private_key_path     = __DIR__.'/config/key/rsa_private_key.pem';
             $mobileAlipay->transport            = config('lxu-alipay.mobileconfig.transport');
             return $mobileAlipay;
         });
